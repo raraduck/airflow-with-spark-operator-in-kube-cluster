a3cf4d567a9774e582a35a36805dbeab0a93e942
Daewoon Kim <raraducker@gmail.com>
2025-11-03 20:58:26 +0900

    update: ml with airbnb data parquet

--- kafka-on-k3d/spark/jobs/ml_spark.py
+++ kafka-on-k3d/spark/jobs/ml_spark.py
@@ -31,3 +31,46 @@ lrModel = lr.fit(vecTrainDF)
 m = round(lrModel.coefficients[0], 2)
 b = round(lrModel.intercept, 2)
 print(f"""The formula for the linear regression line is price = {m}*bedrooms + {b}""")
+
+from pyspark.ml import Pipeline
+pipeline = Pipeline(stages=[vecAssembler, lr])
+pipelineModel = pipeline.fit(trainDF)
+
+predDF = pipelineModel.transform(testDF)
+predDF.select("bedrooms", "features", "price", "prediction").show(10)
+
+from pyspark.ml.feature import OneHotEncoder, StringIndexer
+
+categoricalCols = [field for (field, dataType) in trainDF.dtypes if dataType == "string"]
+indexOutputCols = [x + "Index" for x in categoricalCols]
+oheOutputCols = [x + "OHE" for x in categoricalCols]
+
+stringIndexer = StringIndexer(
+    inputCols=categoricalCols, 
+    outputCols=indexOutputCols,
+    handleInvalid="skip"
+    )
+oheEncoder = OneHotEncoder(
+    inputCols=indexOutputCols,
+    outputCols=oheOutputCols
+    )
+numericCols = [field for (field, dataType) in trainDF.dtypes 
+               if ((dataType == "double") & (field != "price"))]
+assemblerInputs = oheOutputCols + numericCols
+vecAssembler = VectorAssembler(inputCols=assemblerInputs,
+                               outputCol="features")
+
+from pyspark.ml.feature import RFormula
+
+rFormula = RFormula(formula="price ~ .",
+                    featuresCol="features",
+                    labelCol="price",
+                    handleInvalid="skip")
+
+lr = LinearRegression(labelCol="price", featuresCol="features")
+pipeline = Pipeline(stages=[stringIndexer, oheEncoder, vecAssembler, lr])
+# pipeline = Pipeline(stages = [rFormula, lr])
+
+pipelineModel = pipeline.fit(trainDF)
+predDF = pipelineModel.transform(testDF)
+predDF.select("features", "price", "prediction").show(5)
